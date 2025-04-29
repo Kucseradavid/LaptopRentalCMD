b9ca18dccce1707723ab13c3af88ff39226ea737
Kucseradavid <kucseradavid02@gmail.com>
2025-04-29 10:17:41 +0200

    Add project files.

---
+++ LaptopRentalCMD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopRentalCMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Worm!");
+        }
+    }
+}
