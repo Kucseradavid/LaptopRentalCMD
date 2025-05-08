a6f126e935667d6b50959f6caa00826dbe0effe2
Kucseradavid <kucseradavid02@gmail.com>
2025-05-08 12:46:41 +0200

    committeszt

--- LaptopRentalCMD/Program.cs
+++ LaptopRentalCMD/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Worm!");
+            Console.WriteLine("Hello, Worm!"); //committeszt
         }
     }
 }
