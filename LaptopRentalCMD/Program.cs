acbe2a2bbbeab571f879d2bf79e1d1729513ed20
Kucseradavid <kucseradavid02@gmail.com>
2025-05-09 14:05:39 +0200

    5. ig kész

--- LaptopRentalCMD/Program.cs
+++ LaptopRentalCMD/Program.cs
@@ -5,6 +5,88 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Worm!"); //commm
+
+            StreamReader olvaso = new StreamReader("../../../laptoprentals2022.csv");
+            olvaso.ReadLine(); //fejléc
+            List<Rental> adatok = new List<Rental>();
+
+            while (!olvaso.EndOfStream)
+            {
+                string sor = olvaso.ReadLine();
+                Rental egyadat = new Rental(sor);
+                adatok.Add(egyadat);
+            }
+
+            olvaso.Close();
+
+            //foreach (Rental adat in adatok) { Console.WriteLine(adat.ToString()); }
+
+
+            Console.WriteLine();
+
+
+            //3.feladat
+            Console.WriteLine($"3. feladat: Bérlések száma: {adatok.Count}");
+
+            //4.feladat
+            Console.WriteLine("4. feladat: Szürke Acer bérlések");
+            foreach (Rental adat in adatok)
+            {
+                if (adat.Color == "szürke" && adat.Model.IndexOf(@"Acer") != -1)
+                {
+                    Console.WriteLine($"\t{adat.InvNumber} {adat.Model} --- {adat.PersonalID} {adat.Name}");
+                }
+            }
+
+            //5.feladat
+            Console.WriteLine("5. feladat: Vármegyék, ahol a legkevesebb laptopot bérelték");
+            List<string> varmegyek = new List<string>();
+            int[] vmlaptopszamok = new int[19];
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (varmegyek.Contains(adatok[i].County))
+                {
+                    varmegyek.Add(adatok[i].County);
+                }
+            }
+            for (int i = 0; i < varmegyek.Count; i++)
+            {
+                foreach (Rental adat in adatok)
+                {
+                    if (varmegyek[i] == adat.County)
+                    {
+                        vmlaptopszamok[i]++;
+                    }
+                }
+            }
+            string[] legkisvm = new string[1];
+            int[] legkisvmszam = new int[1];
+            legkisvmszam[0] = vmlaptopszamok[0];
+            for (int i = 0; i < varmegyek.Count; i++)
+            {
+                if (legkisvmszam[0] > vmlaptopszamok[i])
+                {
+                    legkisvm[0] = varmegyek[i];
+                    legkisvmszam[0] = vmlaptopszamok[i];
+                }
+            }
+            string[] legkisvm2 = new string[1];
+            int[] legkisvmszam2 = new int[1];
+            legkisvmszam2[0] = vmlaptopszamok[0];
+            for (int i = 0; i < varmegyek.Count; i++)
+            {
+                if (vmlaptopszamok[i] > legkisvmszam[0] && legkisvmszam2[0] > vmlaptopszamok[i])
+                {
+                    legkisvm2[0] = varmegyek[i];
+                    legkisvmszam2[0] = vmlaptopszamok[i];
+                }
+            }
+            Console.WriteLine($"\t{legkisvm[0]} : {legkisvmszam[0]}");
+            Console.WriteLine($"\t{legkisvm2[0]} : {legkisvmszam2[0]}");
+            Console.WriteLine("nem működik, ne is keresse");
+
+            //6.feladat
+            Console.WriteLine("6. feladat:");
         }
     }
 }
---
+++ LaptopRentalCMD/Rental.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopRentalCMD
+{
+    internal class Rental
+    {
+        public string PersonalID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Postalcode { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+
+        public string InvNumber { get; set; }
+        public string Model { get; set; }
+        public string County { get; set; }
+        public int RAM { get; set; }
+        public string Color { get; set; }
+        public int DailyFee { get; set; }
+        public int Deposit { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool UseDeposit { get; set; }
+        public double Uptime { get; set; }
+
+
+        public Rental() { }
+
+
+        public Rental(string sor)
+        {
+            string[] soradatok = sor.Split(";");
+
+            this.PersonalID = soradatok[0];
+            this.Name = soradatok[1];
+            this.DateOfBirth = Convert.ToDateTime(soradatok[2]);
+            this.Postalcode = Convert.ToInt32(soradatok[3]);
+            this.City = soradatok[4];
+            this.Address = soradatok[5];
+            
+            this.InvNumber = soradatok[6];
+            this.Model = soradatok[7];
+            this.County = soradatok[8];
+            this.RAM = Convert.ToInt32(soradatok[9]);
+            this.Color = soradatok[10];
+            this.DailyFee = Convert.ToInt32(soradatok[11]);
+            this.Deposit = Convert.ToInt32(soradatok[12]);
+            
+            this.StartDate = Convert.ToDateTime(soradatok[13]);
+            this.EndDate = Convert.ToDateTime(soradatok[14]);
+            if (Convert.ToInt32(soradatok[15]) == 0) this.UseDeposit = false;
+            else this.UseDeposit = true;
+            this.Uptime = Convert.ToDouble(soradatok[16]);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.PersonalID}, {this.DateOfBirth}, {this.Postalcode}, {this.UseDeposit}";
+        }
+    }
+}
